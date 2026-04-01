3ef09914963cab56476bdf0e69cfbf75c4e9e653
cub-alan <holwill.jacob@gmail.com>
2026-04-01 23:50:26 +0100

    bioclip working but not sam2 yet

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# git clone https://github.com/cub-alan/WDR-AI_Sampling
+# cd WDR-AI_Sampling    
+# python -m venv venv
+# venv\Scripts\activate
+# pip install -r requirements.txt
+
 from transformers import CLIPModel, CLIPProcessor
 
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
