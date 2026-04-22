e785b3495332311b4f3041e377c50ef71049e61a
cub-alan <holwill.jacob@gmail.com>
2026-04-22 16:28:44 +0100

    fixes needs testing

--- main.py
+++ main.py
@@ -7,10 +7,12 @@
 # venv\Scripts\activate
 # pip install -r requirements.txt
 
+#import the libraries needed to run the install requirements function
+import os
 import subprocess
 import sys
     
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"]) # ensure all required libraries are installed before running
 
 import cv2 # open cv library
 import time
@@ -24,7 +26,7 @@ import torch # for Bioclip ai model handling
 from PIL import Image
 from threading import Thread, Lock # for handling multiple image processing threads
 from queue import Queue, Empty, Full # for managing the image processing queue
-
+from flask import Flask, request
 
 #hydra imports
 from hydra import initialize
@@ -37,9 +39,6 @@ from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
 #BioCLIP imports
 from transformers import CLIPModel, CLIPProcessor 
 
-
-
-
 WDR_Webserver_URL = "http://wdr.local:8000" # URL of the WDR webserver to send data to once processed 
 Cam1_URL = "http://192.168.4.138/stream1" # URL of the first camera stream
 Cam2_URL = "http://192.168.4.108/stream2" # URL of the second camera stream
@@ -47,70 +46,123 @@ GNSS_URL = "http://192.168.4.138/status" # URL of the GNSS data stream
 FPS = 20 # Desired frames per second for processing
 Frame_Interval = 1.0 / FPS # Time interval between frames
 
-# create the paths for the SAM2 model config and checkpoint
-SAM_Base_Path = Path.cwd()
-SAM_Config = "sam2.1_hiera_small.yaml" 
-SAM_Check = str(SAM_Base_Path / "sam2.1_hiera_small.pt")
+Sample_Folder = "Sample_Queue" # folder to save the images received from the camera streams and SD card for processing
+os.makedirs(Sample_Folder, exist_ok=True)
 
 # initialize shared variables
 GNSS_New = {"lat": 0, "lon": 0, "valid": 0 , "sats": 0} # variable to get newest gnss data
 GNSS_Lock = Lock() # mutex lock for safely updating the GNSS data variable
-Targets = [] # list of detected weeds
+Targets = ["Dandelion", "Thistle", "Bindweed", "Clover"] # list of detected weeds
 Targets_Lock = Lock() # mutex lock for safely updating targets
-Q_Sample = Queue(maxsize=40) # queue to manage image processing 
+Q_Sample = Queue(maxsize=100) # queue to manage image processing 
+
+# create the paths for the SAM2 model config and checkpoint
+SAM_Config = "sam2.1_hiera_small.yaml" 
+SAM_Check = "sam2.1_hiera_small.pt"
+
+# --- SD CARD IMAGE RECEIVER ---
+flask_app = Flask(__name__)
+
+@flask_app.route('/api/upload_file', methods=['POST'])
+def upload_file():
+    fname = request.headers.get('File-Name', f"upload_{int(time.time())}.jpg")
+    fpath = os.path.join(Sample_Folder, fname)
+    with open(fpath, 'wb') as f:
+        f.write(request.data)
+    print(f"Received SD Image: {fname}")
+    return "OK", 200
+
+def start_receiver():
+    # Use port 5000 so website.py can use 8000
+    flask_app.run(host='0.0.0.0', port=5000)
+
+# --- OFFLINE/SD DATA PROCESSOR ---
+def SD_Receiver():
+    while True:
+        try:
+            files = sorted(os.listdir(Sample_Folder))
+            for f in files:
+                if not f.endswith('.jpg'): continue
+                
+                parts = f.replace('.jpg', '').split('_')
+                if len(parts) < 2: continue
+                
+                cam_name, sample_id = parts[0], parts[1]
+                fpath = os.path.join(Sample_Folder, f)
+                
+                img = cv2.imread(fpath)
+                if img is not None:
+                    sample = {
+                        "frame": img,
+                        "GNSS": {"lat": 0, "lon": 0, "valid": False}, 
+                        "timestamp": f"SD_{sample_id}",
+                        "Cam": cam_name
+                    }
+                    try:
+                        Q_Sample.put(sample, timeout=1)
+                        os.remove(fpath)
+                    except Full:
+                        time.sleep(1)
+        except Exception as e:
+            print(f"Offline Processor error: {e}")
+        time.sleep(2)
 
 
 class Camera_CLASS: # a class to receive and process streams from both cameras
 
-    def Init(CAM,url,name): #function that initializes the camera stream receiver
-        CAM.url = url # URL of the camera stream
-        CAM.name = name # given name for the camera (mostly for debugging purposes)
-        CAM.capture = cv2.VideoCapture(CAM.url) # OpenCV function to get the video streams from the URL
-        CAM.Frame = None # variable for current frame received from the stream
-        CAM.ret = False  # variable to show if frame capture was successfully
-        CAM.lock = Lock() # create a mutex to handle the camera stream
-        CAM.running = True #  flag that controls the stream thread
-        CAM.thread = Thread(target=CAM.Updater, daemon = True) # create the thread and continuously read frames from the stream
-        CAM.thread.start() # start the thread
-    def Updater(CAM): # function that continuously captures frames from the stream
-        while CAM.running: # while the stream is active
-            ret, frame = CAM.capture.read() # read a frame from the stream
-            with CAM.lock: # acquire the mutex lock to safely update the frame variable
-                CAM.ret = ret # update the capture success variable
+    def Init(self,url,name): #function that initializes the camera stream receiver
+        self.url = url # URL of the camera stream
+        self.name = name # given name for the camera (mostly for debugging purposes)
+        self.capture = cv2.VideoCapture(self.url) # OpenCV function to get the video streams from the URL
+        self.Frame = None # variable for current frame received from the stream
+        self.ret = False  # variable to show if frame capture was successfully
+        self.lock = Lock() # create a mutex to handle the camera stream
+        self.running = True #  flag that controls the stream thread
+        self.thread = Thread(target=self.Updater, daemon = True) # create the thread and continuously read frames from the stream
+        self.thread.start() # start the thread
+    def Updater(self): # function that continuously captures frames from the stream
+        while self.running: # while the stream is active
+            ret, frame = self.capture.read() # read a frame from the stream
+            with self.lock: # acquire the mutex lock to safely update the frame variable
+                self.ret = ret # update the capture success variable
                 if ret: # if the frame was successfully captured
-                    CAM.Frame = frame # update the current frame variable
+                    self.Frame = frame # update the current frame variable
                 else:
-                    time.sleep(0.1) # if capture failed, add delay before trying again to allow for queue to continue
-                    CAM.capture.release() # remove the current capture as it didnt retreive correctly
-                    CAM.capture = cv2.VideoCapture(CAM.url) # reinitialize capture to attempt to fix the retrieval error
-    def Get_Frame(CAM): # function to retrieve the current frame from the stream
-        with CAM.lock: # acquire the mutex lock to safely access the frame variable
-            if CAM.ret and CAM.Frame is not None: # if the last capture was successful
-                return True, CAM.Frame.copy() # return the current frame
+                    time.sleep(1) # if capture failed, add delay before trying again to allow for queue to continue
+                    self.capture.release() # remove the current capture as it didnt retreive correctly
+                    self.capture = cv2.VideoCapture(self.url) # reinitialize capture to attempt to fix the retrieval error
+
+    def Get_Frame(self): # function to retrieve the current frame from the stream
+        with self.lock: # acquire the mutex lock to safely access the frame variable
+            if self.ret and self.Frame is not None: # if the last capture was successful
+                return True, self.Frame.copy() # return the current frame
             else: # if capture failed
                 return False, None # return empty
             
 def Init_Libs(): # function to initialize the SAM2 and BioCLIP models
+
+    print("\nVerifying AI Models...")
+    if not os.path.exists(SAM_Check):
+        print(f"ERROR: {SAM_Check} missing! Place it in the root folder.")
+        sys.exit(1)
 
     if GlobalHydra.instance().is_initialized(): # check if hydra has been initialised
         GlobalHydra.instance().clear() # if initialised clear hydra to stop any errors 
 
     initialize(config_path="configs", version_base=None) # initialize hydra for SAM2
 
-    Hardware = "cpu" # set device to cpu to avoid cuda errors
+    Device = "cpu" # set device to cpu to avoid cuda errors
 
-    sam2 = build_sam2(
-        config_file=SAM_Config, ckpt_path=str(SAM_Check), device = Hardware # build the sam 2 model using the config/checkpoint + use cpu to avoid using cuda 
-    )
+    sam2 = build_sam2(config_file=SAM_Config, ckpt_path=str(SAM_Check), device = Device) # build the sam 2 model using the config/checkpoint + use cpu to avoid using cuda 
 
     SAM_Mask = SAM2AutomaticMaskGenerator(sam2) 
 
-    Bio_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(Hardware) # load the BioCLIP model and set to cpu to avoid cuda errors
+    Bio_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(Device) # load the BioCLIP model and set to cpu to avoid cuda errors
     Bio_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
     print("SAM2 and BioCLIP INIT Complete")
 
-    return SAM_Mask, Bio_model, Bio_processor, Hardware
+    return SAM_Mask, Bio_model, Bio_processor, Device
 
 def Get_GNSS(): # function to continuously get the latest GNSS data from the stream
     global GNSS_New
@@ -120,12 +172,10 @@ def Get_GNSS(): # function to continuously get the latest GNSS data from the str
             
             GNSS_Data = GNSS_Receive.json() # parse the response as JSON
             with GNSS_Lock: # user the gnss mutex to safely retreive the gnss data
-                GNSS_New["lat"] = GNSS_Data.get("lat", 0) # get the newest latitude value
-                GNSS_New["lon"] = GNSS_Data.get("lon", 0) # get the newest longitude value
-                GNSS_New["valid"] = GNSS_Data.get("valid", 0) # get the newest validity status
-                GNSS_New["sats"] = GNSS_Data.get("sats", 0) # get the newest value of satellites locked
+                GNSS_New.update(GNSS_Data) # update the global variable with the new gnss data
         except Exception as error:
             print(error) # if there is an error skip and then try again
+            pass
         time.sleep(1) # add a delay before trying again to avoid overwhelming the server
 
 def Plant_Filter():
@@ -133,11 +183,12 @@ def Plant_Filter():
     while True:
         try:
             response = requests.get(f"{WDR_Webserver_URL}/filters", timeout=5) # get the filters from the webserver 
-            filters = response.json() # parse the response as JSON
-            with Targets_Lock: # use the targets mutex to safely update the targets list
-                Targets = filters # update the targets list with the new filters
+            if response.status_code == 200:
+                with Targets_Lock: # use the targets mutex to safely update the targets list
+                    Targets = response.json() # update the targets list with the new filters
         except Exception as error:
             print(error) # if there is an error skip and then try again
+            pass
         time.sleep(2) # add a delay before trying again to avoid overwhelming the server
 
 def Sample_Process(Stream1,Stream2):       
@@ -156,10 +207,12 @@ def Sample_Process(Stream1,Stream2):
                     Q_Sample.put_nowait(Sample) # add the sample to the processing queue without blocking
                 except Full:
                     pass
+
         Time_Diff = time.time() - start_time # calculate the time taken to process the frame
         time.sleep(max(0, Frame_Interval - Time_Diff)) # add a delay to maintain the desired frame rate
 
 def Frame_Process(frame, SAM_Mask, Bio_model, Bio_processor, device, Targets):
+
     if not SAM_Mask or not Bio_model or not Targets: # check if the models are initialized
         return []
     
@@ -188,7 +241,7 @@ def Frame_Process(frame, SAM_Mask, Bio_model, Bio_processor, device, Targets):
             detected_targets.append({"label": label, "confidence": confidence, "bbox": mask["bbox"]}) # add the detected target to the list with its label, confidence, and bounding box
     return detected_targets # return the list of detected targets
 
-def AI_Loop(SAM_Mask, Bio_model, Bio_processor, Hardware):
+def AI_Loop(SAM_Mask, Bio_model, Bio_processor, Device):
     while True:
         try:
             Sample = Q_Sample.get(timeout=1) # get a sample from the processing queue with a timeout
@@ -208,7 +261,7 @@ def AI_Loop(SAM_Mask, Bio_model, Bio_processor, Hardware):
             else:
                 TimeStamp = Sample["timestamp"] # if GNSS data is not valid, use the sample timestamp
             
-            Detected_Targets = Frame_Process(frame, SAM_Mask, Bio_model, Bio_processor, Hardware, Current_Targets) # process the frame to detect targets
+            Detected_Targets = Frame_Process(frame, SAM_Mask, Bio_model, Bio_processor, Device, Current_Targets) # process the frame to detect targets
 
             labels_list = []
 
@@ -247,12 +300,14 @@ def main():
     Cam2 = Camera_CLASS()
 
     # initialize the camera streams with the urls set above
-    Cam1.Init(Cam1,Cam1_URL,"Cam1")
-    Cam2.Init(Cam2,Cam2_URL,"Cam2")
+    Cam1.Init(Cam1_URL,"Cam1")
+    Cam2.Init(Cam2_URL,"Cam2")
 
     #start all threads
     Thread(target=Get_GNSS, daemon=True).start() # start the thread to continuously get the latest GNSS data
     Thread(target=Plant_Filter, daemon=True).start() # start the thread to continuously get the latest plant filters from the webserver
+    Thread(target=start_receiver, daemon=True).start()
+    Thread(target=SD_Receiver, daemon=True).start()
     Thread(target=Sample_Process, args=(Cam1, Cam2), daemon=True).start() # start the thread to continuously process the camera samples
 
     AI_Loop(SAM_Mask, Bio_model, Bio_processor, device)
